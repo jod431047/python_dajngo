7ccc6335f26c5a443204fc942ea16cfbdbbad1fe
jod431047 <jod431047@gmail.com>
2023-09-19 21:08:25 +0200

    added post_list & post_detail in view

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import *
+from django.views import generic
 # Create your views here.
+
+
+class PostList(generic.ListView):
+    model  = Course
+    
+
+
+class PostDetail(generic.DetailView):
+    model  = Course
