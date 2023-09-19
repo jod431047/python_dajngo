f8eeb65544741a0bb0f58e539863466e23c0f3eb
jod431047 <jod431047@gmail.com>
2023-09-19 20:24:52 +0200

    add models in admin

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# test git
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
